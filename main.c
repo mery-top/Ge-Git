48582b66d2a1f110474398871880115712238d22
Meerthika <meerthikasrrajendrans@gmail.com>
2025-07-03 18:19:42 +0530

    [Add] commit and tree_hash to main.c

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include "include/init.h"
 #include "include/hash_object.h"
+#include "include/tree.h"
+#include "include/commit.h"
 
 int main(int argc, char* argv[]){
     if(argc <2){
@@ -11,12 +13,32 @@ int main(int argc, char* argv[]){
 
     if(strcmp(argv[1], "init") == 0){
         git_init();
-        return 0;
     }else if(strcmp(argv[1], "hash-object") == 0){
         hash_object("hello.txt");
-        return 0;
+    }else if(strcmp(argv[1], "write-tree") == 0){
+        char sha1[41];
+        write_tree(".", sha1);
+        printf("tree: %s\n", sha1);
+    }else if(strcmp(argv[1], "commit") == 0){
+        if(argv <3){
+            printf("Usage: git commit <message>\n");
+            return 1;
+        }
+
+        char tree_hash[41];
+        write_tree(".", tree_hash);
+        char commit_hash[41];
+
+        write_commit(tree_hash, argv[2], commit_hash);
+        printf("commit: %s\n", commit_hash);
+    }eles{
+        printf("Unknown command: %s\n", argv[1]);
+        return 1;
     }
 
-    printf("Unknown command: %s\n", argv[1]);
-    return 1;
+    
+
+    return 0;
 }
+
+// printf("Unknown command: %s\n", argv[1]);
